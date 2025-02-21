699c6c0db27cace1ef4d48f700d318b9782f816f
Hugo Piat <h.piat@outlook.fr>
2025-02-21 15:44:30 +0100

    Fixed exception

--- src/main/java/com/arkaitem/items/MenuItemsGUIListener.java
+++ src/main/java/com/arkaitem/items/MenuItemsGUIListener.java
@@ -2,6 +2,7 @@ package com.arkaitem.items;
 
 import com.arkaitem.Program;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,21 +19,24 @@ public class MenuItemsGUIListener implements Listener {
         Inventory inventory = event.getInventory();
 
         if (inventory.getTitle().equalsIgnoreCase(MenuItemsGUI.TITLE)) {
-            event.setCancelled(true);
 
             ItemStack clickedItem = event.getCurrentItem();
 
-            Player player = (Player) event.getWhoClicked();
-            String itemName = clickedItem.getItemMeta().getDisplayName();
+            if (clickedItem != null && !ItemsUtils.areEquals(clickedItem, new ItemStack(Material.AIR))) {
 
-            Optional<CustomItem> item = Program.INSTANCE.ITEMS_MANAGER.getItemByDisplayName(itemName);
+                Player player = (Player) event.getWhoClicked();
+                String itemName = clickedItem.getItemMeta().getDisplayName();
 
-            if (!item.isPresent()) {
-                throw new IllegalStateException("No custom item found with display name: " + itemName);
+                Optional<CustomItem> item = Program.INSTANCE.ITEMS_MANAGER.getItemByDisplayName(itemName);
+
+                if (!item.isPresent()) {
+                    throw new IllegalStateException("No custom item found with display name: " + itemName);
+                }
+
+                String command = "arkaitem give " + item.get().getId() + " " + player.getName();
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+                event.setCancelled(true);
             }
-
-            String command = "arkaitem give " + item.get().getId() + " " + player.getName();
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
         }
     }
 }
